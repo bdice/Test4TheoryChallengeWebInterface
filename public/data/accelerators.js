c55471a817e5721cb3fd138898089d4013cbdf76
S.P. Mohanty <spmohanty91@gmail.com>
2014-12-07 15:44:42 +0100

    Changing default photo for timeline entries to cern 60 logo

--- public/data/accelerators.js
+++ public/data/accelerators.js
@@ -36,14 +36,14 @@ var TOTAL = {
 					   {
 					      "headline":"First observations of antinuclei",
 					      "date":"1965,9,1",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>By 1965, all three particles that make up atoms (electrons, protons and neutrons) were known to each have an antiparticle. So if particles, bound together in atoms, are the basic units of matter, it is natural to think that antiparticles, bound together in antiatoms, are the basic units of antimatter.</p>\n<p>But are matter and antimatter exactly equal and opposite, or symmetric, as Dirac had implied? The next important step was to test this symmetry. Physicists wanted to know how subatomic antiparticles behave when they come together. Would an antiproton and an antineutron stick together to form an antinucleus, just as protons and neutrons stick together to form the nucleus of an atom?</p>\n<p>The answer to the antinuclei question was found in 1965 with the observation of the antideuteron, a nucleus of antimatter made out of an antiproton plus an antineutron (while a deuteron &ndash; the nucleus of the deuterium atom &ndash; is made of a proton plus a neutron). The goal was simultaneously achieved by two teams of physicists, one led by Antonino Zichichi using the Proton Synchrotron at CERN, and the other led by Leon Lederman, using the Alternating Gradient Synchrotron (AGS) accelerator at the Brookhaven National Laboratory, New York.</p>\n<p>The CERN paper, <a href=\"http://www.springerlink.com/content/ch673h285360045w/\"><em>Experimental Observation of Antideuteron Production</em></a> was published in the Italian particle-physics journal <em>Il nuovo cimento</em> on 1 September 1965 (the journal ended when it was merged into the <em>European Physical Journal</em> in 1999.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"Georges Charpak revolutionizes detection",
 					      "date":"1968,1,17",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>In the 1960s, detection in particle physics mainly involved examining millions of photographs from bubble chambers or spark chambers. This was slow, labour-intensive and unsuitable for studies into rare phenomena.</p>\n<p>Then came a revolution in transistor amplifiers. While a camera can detect a spark, a detector wire connected to an amplifier can detect a much smaller effect. In 1968, Georges Charpak developed the &ldquo;multiwire proportional chamber&rdquo;, a gas-filled box with a large number of parallel detector wires, each connected to individual amplifiers. Linked to a computer, it could achieve a counting rate a thousand times better than existing detectors. The invention revolutionized particle detection, which passed from the manual to the electronic era.</p>\n<p>Charpak, who joined CERN in 1959, was awarded the <a href=\"http://www.nobelprize.org/nobel_prizes/physics/laureates/1992/\">1992 Nobel prize in physics</a>&nbsp;\"for his invention and development of particle detectors, in particular the multiwire proportional chamber\".</p>\n<p>Today practically every experiment in particle physics uses some track detector based on the principle of the multiwire proportional chamber. Charpak has also actively contributed to the use of this technology in other fields that use ionizing radiation such as biology, radiology and nuclear medicine.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -57,7 +57,7 @@ var TOTAL = {
 					   {
 					      "headline":"Council commissions the Super Proton Synchrotron",
 					      "date":"1971,2,10",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>Seven kilometres in circumference, the Super Proton Synchrotron (SPS) was the first of CERN&rsquo;s giant underground rings. It was also the first accelerator to cross the Franco&ndash;Swiss border.</p>\n<p>Eleven of CERN's member states approved the construction of the SPS in February 1971, and it was switched on for the first time on 17 June 1976, two years ahead of schedule. The SPS quickly became the workhorse of CERN&rsquo;s particle physics programme, providing beams to two large experimental areas. Advances in technology during the building period meant that not only was construction finished early, it was able to operate with a beam energy of 400 GeV - 100 GeV higher than the original design energy.</p>\n<p>The SPS operates today at up to 450 GeV, and has handled many different kinds of particles. Research using SPS beams has probed the inner structure of protons, investigated nature&rsquo;s preference for matter over antimatter, looked for matter as it might have been in the first instants of the universe and searched for exotic forms of matter.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -78,7 +78,7 @@ var TOTAL = {
 					   {
 					      "headline":"First proton-antiproton collisions",
 					      "date":"1981,4,4",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>The Intersecting Storage Rings produced the world&rsquo;s first proton-antiproton collisions on 4 April 1981, paving the way for proton-antiproton collisions in the Super Proton Synchrotron (SPS), and the <a href=\"http://www.nobelprize.org/nobel_prizes/physics/laureates/1984/\">Nobel prize</a> for <a href=\"http://www.nobelprize.org/nobel_prizes/physics/laureates/1984/meer.html\">Simon van der Meer</a> and <a href=\"http://www.nobelprize.org/nobel_prizes/physics/laureates/1984/rubbia.html\">Carlo Rubbia</a>.</p>\n<p>The ISR proved to be an excellent instrument for particle physics. By the time the machine closed down in 1984, it had produced many important results, including indications that protons contain smaller constituents, ultimately identified as quarks and gluons.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -92,7 +92,7 @@ var TOTAL = {
 					   {
 					      "headline":"Heavy-ion collisions begin",
 					      "date":"1986,6,11",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>Just after the big bang the universe was too hot and dense for the existence of familiar particles such as protons and neutrons. Instead, their constituents &ndash; the quarks and gluons &ndash; roamed freely in a \"particle soup\" called quark-gluon plasma.</p>\n<p>In 1986 CERN began to accelerate heavy ions &ndash; nuclei containing many neutrons and protons &ndash; in the Super Proton Synchrotron (SPS) to study the possibility that quark gluon-plasma was more than just a theory. The aim was to \"deconfine\" quarks &ndash; set them free from their confinement within atoms - by smashing the heavy ions into appropriate targets.</p>\n<p>The first experiments used relatively light nuclei such as oxygen and sulphur, and produced results consistent with the quark-gluon plasma theory, but no real proof. In 1994 a second generation of experiments began with lead ions, and by 2000 there was <a href=\"http://press.web.cern.ch/press-releases/2000/02/new-state-matter-created-cern\">compelling evidence</a> that a new state of matter had been seen.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -120,14 +120,14 @@ var TOTAL = {
 					   {
 					      "headline":"First antiatoms produced: antihydrogen, at CERN",
 					      "date":"1995,9,15",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>A team led by Walter Oelert created atoms of antihydrogen for the first time at CERN&rsquo;s Low Energy Antiproton Ring (LEAR) facility. Nine of these atoms were produced in collisions between antiprotons and xenon atoms over a period of 3 weeks. Each one remained in existence for about 40 billionths of a second, travelled at nearly the speed of light over a path of 10 metres and then annihilated with ordinary matter. The annihilation produced the signal that showed that the anti-atoms had been created.</p>\n<p>This was the first time that antimatter particles had been brought together to make complete atoms, and the first step in a programme to make detailed measurements of antihydrogen.</p>\n<p>The hydrogen atom is the simplest atom of all, made of a single proton orbited by an electron. Some three quarters of all the ordinary matter in the universe is hydrogen, and the hydrogen atom is one of the best understood systems in physics. Comparison with antihydrogen offers a route to understanding the matter&ndash;antimatter asymmetry in the universe.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"CMS and ATLAS experiments approved",
 					      "date":"1997,1,31",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p><span>Four years after the first technical proposals, the experiments CMS and ATLAS were officially approved. </span>Both are general-purpose experiments designed to explore the fundamental nature of matter and the basic forces that shape our universe, including the Higgs boson<span>.</span></p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -141,21 +141,21 @@ var TOTAL = {
 					   {
 					      "headline":"ALICE experiment approved",
 					      "date":"1997,2,14",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>The CERN research board&nbsp;<a href=\"http://committees.web.cern.ch/committees/RB/RBMinutes/RBMinutes130.html\">officially approves</a> the ALICE experiment. Re-using the L3 magnet experiment from the LEP, ALICE is designed to study quark-gluon plasma, a state of matter that would have existed in the first moments of the universe.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"LHCb experiment approved",
 					      "date":"1998,9,17",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p><span>LHCb is the fourth experiment <a href=\"http://committees.web.cern.ch/committees/RB/RBMinutes/RBMinutes137.html\">approved</a> for the LHC. The experiment will study the phenomenon known as CP violation, which would help to explain why matter dominates antimatter in the universe.</span></p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"LEP's final shutdown",
 					      "date":"2000,11,2",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>The Large Electron-Positron collider was shut down for the last time at 8am on 2 November 2000. Members of government from around the world gathered at CERN on 9 October to celebrate the achievements of LEP and its 11 years of operational life. With the tunnel now &nbsp;available for work, teams began excavating the caverns to house the four big detectors on the Large Hadron Collider.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
@@ -183,35 +183,35 @@ var TOTAL = {
 					   {
 					      "headline":"Incident at the LHC",
 					      "date":"2008,9,19",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>On 19 September 2008, during powering tests of the main dipole circuit in Sector 3-4 of the LHC, a fault occurred in the electrical bus connection in the region between a dipole and a quadrupole, resulting in mechanical damage and release of helium from the magnet cold mass into the tunnel. Proper safety procedures were in force, the safety systems performed as expected, and no-one was put at risk.</p>\n<h3>More about the incident:&nbsp;</h3>\n<p>A full technical analysis of the incident is <a href=\"http://edms.cern.ch/file/973073/1/Report_on_080919_incident_at_LHC__2_.pdf\">available here</a>.&nbsp;</p>\n<p>Or read an analysis of the LHC incident on <a href=\"http://press.web.cern.ch/press-releases/2008/10/cern-releases-analysis-lhc-incident\">CERN's press office website</a>.&nbsp;</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"Final magnet goes underground after LHC repair",
 					      "date":"2009,4,30",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>The 53rd and final replacement magnet for the Large Hadron Collider (LHC) was lowered into the accelerator's tunnel today, marking the end of repair work above ground following the incident in September last year that brought LHC operations to a halt.</p>\n<p>The final magnet, a quadrupole designed to focus the beam, was lowered in the afternoon and started its journey to Sector 3-4, the scene of the September incident. In total 53 magnets were removed from Sector 3-4. Sixteen that sustained minimal damage were refurbished and put back into the tunnel. The remaining 37 were replaced and will be refurbished to provide spares for the future.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"ALPHA traps antimatter atoms for 1000 seconds",
 					      "date":"2011,6,5",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>The ALPHA experiment at CERN reported today that it succeeded in trapping antimatter atoms for over 16 minutes: long enough to begin to study their properties in detail. ALPHA is part of a broad programme at CERN&rsquo;s antiproton decelerator investigating the mysteries of one of nature&rsquo;s most elusive substances.</p>\n<p>ALPAH studied 300 trapped antiatoms. Trapping antiatoms will allow antihydrogen to be mapped precisely using laser or microwave spectroscopy so that it can be compared to the hydrogen atom, which is among the best-known systems in physics. Any difference between matter and antimatter should become apparent under careful scrutiny.</p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"Tantalising hints of the Higgs",
 					      "date":"2011,12,13",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>In a seminar today the ATLAS and CMS experiments presented the status of their searches for the Standard Model Higgs boson. Their results were based on the analysis of considerably more data than those presented at the summer conferences, enough to make significant progress in the search for the Higgs boson, but not enough to make any conclusive statement on the existence or non-existence of the elusive Higgs. The main conclusion was that the Standard Model Higgs boson, if it exists, is most likely to have a mass constrained to the range 116-130 GeV by the ATLAS experiment, and 115-127 GeV by CMS. Tantalising hints were seen by both experiments in this mass region, but they were not yet strong enough to claim a discovery.</p>\n<p>&nbsp;</p>",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   },
 					   {
 					      "headline":"ATLAS and CMS observe particle consistent with the Higgs boson",
 					      "date":"2012,7,4",
-					      "media":"https://c2.staticflickr.com/6/5087/5248565279_56c7a0d787_z.jpg",
+					      "media":"http://design-guidelines.web.cern.ch/sites/design-guidelines.web.cern.ch/files/u877/60_1.png",
 					      "description":"<p>At a seminar held at CERN on 4 July 2012 as a curtain raiser to the year&rsquo;s major particle physics conference, ICHEP2012 in Melbourne, the ATLAS and CMS experiments presented their latest preliminary results in the search for the long sought Higgs particle. Both experiments observe a new particle in the mass region around 125-126 GeV.</p>\n<p><a href=\"http://press.web.cern.ch/press-releases/2012/07/cern-experiments-observe-particle-consistent-long-sought-higgs-boson\">Read the press release</a></p>\n",
 					      "icon":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png"
 					   }
